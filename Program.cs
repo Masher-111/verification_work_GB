b1e4d50e68749feb45ac189c3e208642139e2110
Masher-111 <90869108+Masher-111@users.noreply.github.com>
2022-01-03 13:15:09 +0300

    Создан метод вывода в консоль массива

--- Program.cs
+++ Program.cs
@@ -6,3 +6,12 @@
         array[i] = new Random().Next(-10, 10); 
     }
 }
+
+void PrintArray (int [] array)
+{
+    int length = array.Length;
+    for (int position = 0; position < length; position++)
+    {
+        Console.Write (array[position] + " ");
+    }
+}
